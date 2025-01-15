5a9ccfeac8ae7f3e21ce79e74f26ddc2f5c64be6
andycoode <ajcoode@yahoo.co.uk>
2025-01-15 22:02:52 +0000

    Breakdown some of the if statements into manageable chunks for Aged Brie and Backstage passes

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -21,26 +21,29 @@ namespace GildedRoseKata
                         item.Quality -= 1;
                     }
                 }
-                else
+
+                if (item.Name == "Aged Brie" && item.Quality < 50)
                 {
-                    if (item.Quality < 50)
+                    item.Quality += 1;
+                }
+                
+              
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Quality < 50)
+                {
+                    item.Quality += 1;
+                
+                    if (item.SellIn < 11 && item.Quality < 50)
                     {
                         item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11 && item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                            
-                            if (item.SellIn < 6 && item.Quality < 50)
-                            {
-                                item.Quality += 1;
-                            }
-                        }
                     }
+                    
+                    if (item.SellIn < 6 && item.Quality < 50)
+                    {
+                        item.Quality += 1;
+                    }
+                    
                 }
+                
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
