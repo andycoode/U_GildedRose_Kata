8b6871e728368e92be87e978ece453c587651f5b
andycoode <ajcoode@yahoo.co.uk>
2025-01-15 22:37:43 +0000

    Broken down the remaining into Sulfras and everything else.  Test all still passing

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -23,29 +23,15 @@ namespace GildedRoseKata
                     case "Backstage passes to a TAFKAL80ETC concert":
                         BackstagePasses(item);
                         break;
+                    case "Sulfuras, Hand of Ragnaros":
+                        Sulfuras(item);
+                        break;
+                    default:
+                        OtherItems(item);
+                        break;
 
                 }
 
-
-
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality -= 1;
-                }
-
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Aged Brie")
-                {
-                    item.SellIn -= 1;
-                }
-
-                if (item.SellIn < 0 && item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.Quality -= 1;
-                }
-
-                
-                
             }
         }
 
@@ -74,6 +60,28 @@ namespace GildedRoseKata
             if (item.SellIn < 0 && item.Quality < 50)
             {
                 item.Quality += 1;
+            }
+        }
+
+        private void Sulfuras(Item item)
+        {
+            //Never changes
+            item.Quality = item.Quality;
+            item.SellIn = item.SellIn;
+        }
+
+        private void OtherItems(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality -= 1;
+            }
+
+            item.SellIn -= 1;
+            
+            if (item.SellIn < 0 && item.Quality > 0)
+            {
+                item.Quality -= 1;
             }
         }
     }
