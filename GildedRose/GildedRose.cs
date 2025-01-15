733bf3a7474b8003df0a5c45e3617f39ac590382
andycoode <ajcoode@yahoo.co.uk>
2025-01-15 22:32:32 +0000

    Break out the aged brie into a new function, create a switch case to easily pick the correct function

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -14,40 +14,31 @@ namespace GildedRoseKata
         {
             foreach (var item in _items)
             {
+
+                switch (item.Name)
+                {
+                    case "Aged Brie":
+                        AgedBrie(item);
+                        break;
+                    case "Backstage passes to a TAFKAL80ETC concert":
+                        BackstagePasses(item);
+                        break;
+
+                }
+
+
+
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                 {
                     item.Quality -= 1;
                 }
 
-                if (item.Name == "Aged Brie")
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
-                    item.SellIn -= 1;
-                    
-                    if (item.SellIn < 0 &&item.Quality < 50)
-                    {
-                        item.Quality += 1;
-                    }
-                }
-               
-
-
-                //BACKSTAGE LOGIC ALL HERE!
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    BackstagePasses(item);
-                }
-                
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Aged Brie")
                 {
                     item.SellIn -= 1;
                 }
 
-               
                 if (item.SellIn < 0 && item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros")
                 {
                     item.Quality -= 1;
@@ -70,6 +61,20 @@ namespace GildedRoseKata
             item.SellIn -= 1;
 
             if (item.SellIn < 0) item.Quality = 0;
+        }
+
+        private void AgedBrie(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
+            item.SellIn -= 1;
+
+            if (item.SellIn < 0 && item.Quality < 50)
+            {
+                item.Quality += 1;
+            }
         }
     }
 }
