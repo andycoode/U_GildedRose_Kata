75d67b693afd20cdd7a33245700227e286a472ff
andycoode <ajcoode@yahoo.co.uk>
2025-01-15 17:27:50 +0000

    Changes to unit test structure for first 3 acceptance tests. Create base class which can be used for repetative functions

---
+++ GildedRoseTests/GildedRoseAcceptanceTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class GildedRoseAcceptanceTests : GildedRoseTestBase
+{
+    [Fact]
+    public void Ensure_Item_Has_Sellin_Property()
+    {
+        //Arrange
+        var res = typeof(Item);
+        var propertyNameLower = "sellin";
+
+        //Act
+        var property = res
+            .GetProperties()
+            .FirstOrDefault(p => p.Name.ToLower() == propertyNameLower);
+
+        //Assert
+        Assert.NotNull(property);
+    }
+
+    [Fact]
+    public void Ensure_Item_Has_Quality_Property()
+    {
+        //Arrange
+        var res = typeof(Item);
+        var propertyNameLower = "quality";
+
+        //Act
+        var property = res
+            .GetProperties()
+            .FirstOrDefault(p => p.Name.ToLower() == propertyNameLower);
+
+        //Assert
+        Assert.NotNull(property);
+    }
+
+    [Fact]
+    public void Ensure_Quality_And_SellIn_Are_Lowered_By_One()
+    {
+        //Arrange
+        var items = CreateItems("foo", 5, 3);
+        GildedRose app = new GildedRose(items);
+
+        const string expectedName = "foo";
+        const int expectedSellIn = 4;
+        const int expectedQuality = 2;
+
+        //Act
+        app.UpdateQuality();
+
+        //Assert
+        AssertResults(items, expectedName, expectedSellIn, expectedQuality);
+    }
+}
---
+++ GildedRoseTests/GildedRoseTestBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTestBase
+{
+    public List<Item> CreateItems(string name, int sellin, int quality)
+    {
+        return new List<Item> { new Item { Name = name, SellIn = sellin, Quality = quality } };
+    }
+
+    public void AssertResults(List<Item> items, string itemName, int expectedSellinValue, int expectedQualityValue)
+    {
+        Assert.Equal(itemName, items[0].Name);
+        Assert.Equal(expectedSellinValue, items[0].SellIn);
+        Assert.Equal(expectedQualityValue, items[0].Quality);
+    }
+}
